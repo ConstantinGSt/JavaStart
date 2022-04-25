949d348638badc63f9d86382aeea0ad75ab7e810
ConstantinGSt <dinscalin.ks@gmail.com>
2022-04-25 17:20:14 +0700

    add jaeger

---
+++ src/lesson2/Jaeger.java
@@ -0,0 +1,28 @@
+package lesson2;
+
+public class Jaeger {
+	String generation;
+	String modelName;
+	String color;
+	double weight;
+	double tonnage;
+	double speed;
+	int requirePilots;
+	
+	
+	public void run() {
+		System.out.println(modelName + " run");
+	}
+	
+	public void eat() {
+		System.out.println(modelName + " scan area");
+	}
+	
+	public void sleep() {
+		System.out.println(modelName + " seet");
+	}
+	
+	public void learningJava() {
+		System.out.println(modelName + " Fight");
+	}
+}
