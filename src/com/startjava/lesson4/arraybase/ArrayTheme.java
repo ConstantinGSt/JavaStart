be91297524b3e92f742e2261f22723f25b6d8fd9
Constantin St <dinscalin.ks@gmail.com>
2022-06-14 08:35:11 +0700

    finish first task for Lesson4

--- src/com/startjava/lesson4/arraybase/ArrayTheme.java
+++ src/com/startjava/lesson4/arraybase/ArrayTheme.java
@@ -2,11 +2,18 @@ package com.startjava.lesson4.arraybase;
 
 public class ArrayTheme {
     public static void main(String[] args) {
-        int [] array = {1, 2, 3, 4, 5, 6, 7};
-        int [] yarra = new int [6];
-        int i = 0;
-        for(int ar : array) {
-            System.out.println("array[" + i + "]" + " = " + ar);
+        int[] ar1 = {1, 2, 3, 4, 5, 6, 7};
+        int[] ar2 = new int [7];
+        int z = ar2.length - 1 ;
+        int x = 0;
+        for(int i = 0; i < ar1.length; i++) {
+            System.out.println("ar1[" + i + "]" + " = " + ar1[i]);
+            ar2[z] = ar1[i];
+            z--;
+
+        }
+        for(int ar : ar2) {
+            System.out.println("ar2[" + x++ + "]" + " = " + ar);
         }
     }
 }
