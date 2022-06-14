45179a1056882266f942c16118c94aee522ce72e
Constantin St <dinscalin.ks@gmail.com>
2022-06-14 07:36:33 +0700

    add first array

--- src/com/startjava/lesson4/arraybase/ArrayTheme.java
+++ src/com/startjava/lesson4/arraybase/ArrayTheme.java
@@ -1,7 +1,12 @@
-package com.strtjava.lesson4.arraybase;
+package com.startjava.lesson4.arraybase;
 
 public class ArrayTheme {
     public static void main(String[] args) {
-        
+        int [] array = {1, 2, 3, 4, 5, 6, 7};
+        int [] yarra = new int [6];
+        int i = 0;
+        for(int ar : array) {
+            System.out.println("array[" + i + "]" + " = " + ar);
+        }
     }
 }
