2266c9b084b8dcd72ca88b4edd06bd907fc9b21b
Constantin St <dinscalin.ks@gmail.com>
2022-06-19 11:32:24 +0700

    merge ArrayTheme to lesson_2_3_4

--- src/com/startjava/lesson_2_3_4/arraybase/ArrayTheme.java
+++ src/com/startjava/lesson_2_3_4/arraybase/ArrayTheme.java
@@ -1,4 +1,4 @@
-package com.startjava.lesson4.arraybase;
+package com.startjava.lesson_2_3_4.arraybase;
 
 public class ArrayTheme {
     public static void main(String[] args) {
@@ -7,6 +7,7 @@ public class ArrayTheme {
         int[] ar2 = new int [7];
         int z = ar2.length - 1 ;
         int x = 0;
+        int sum = 1;
         for(int i = 0; i < ar1.length; i++) {
             System.out.println("ar1[" + i + "]" + " = " + ar1[i]);
             ar2[z] = ar1[i];
@@ -16,5 +17,37 @@ public class ArrayTheme {
             System.out.println("ar2[" + x++ + "]" + " = " + ar);
         }
         System.out.println("");
+
+        System.out.println("2. Вывод произведения элементов массива");
+        int[] list = new int [10];
+        for(int i =0; i < list.length; i++) {
+            list[i] = i;
+            if(list[i] > 0 && list[i] < 9) {
+                sum *=list[i];
+            } 
+        } System.out.println("без учета первого и последнего индекса  = " + sum + " \n");
+
+        System.out.println("3. Удаление элементов массива");
+        double[] list3 = new double[15];
+        double[] list3_1 = new double[15];
+        for(int i=0; i < list3.length; i++) {
+            list3[i] = Math.random();
+            System.out.println(list3[i]);
+        }
+        System.out.println("Удаление всех больше индекс 7");
+        for(int i = 0; i < list3.length; i++) {
+            if(list3[i] > list3[7]) {
+                list3_1[i] = 0;
+            } else list3_1[i] = list3[i];
+        }
+        for(double q : list3_1) {
+            System.out.println(q);
+        } System.out.println("");
+
+        System.out.println("4. Вывод элементов массива лесенкой в обратном порядке");
+            for(char i = 'A'; i<='Z'; i++)
+        {
+            System.out.println(i);
+        }
     }
 }
