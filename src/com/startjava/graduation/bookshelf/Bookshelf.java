a22a3c64d3f7152690fa7f9ae0c837e5c811182d
ConstantinGSt <stepanyuk.kon@yandex.ru>
2022-07-03 16:04:22 +0700

    construct first skellt

--- src/com/startjava/graduation/bookshelf/Bookshelf.java
+++ src/com/startjava/graduation/bookshelf/Bookshelf.java
@@ -1,5 +1,22 @@
 package com.startjava.graduation.bookshelf;
 
 public class Bookshelf {
-    
+
+	Books[] bookShelf = new Books[10];
+    public MyBookShelf(int inpitMenuOption) {
+    	if(inpitMenuOption == 1) {
+
+    	} else if(inpitMenuOption == 2) {
+
+    	} else if(inpitMenuOption == 3) {
+
+    	} else if(inpitMenuOption == 4) {
+
+    	} else if(inpitMenuOption == 5) {
+
+    	} else if(inpitMenuOption == 6) {
+    		
+    	}
+    }
+
 }
--- src/com/startjava/graduation/bookshelf/BookshelfTest.java
+++ src/com/startjava/graduation/bookshelf/BookshelfTest.java
@@ -1,11 +1,27 @@
 package com.startjava.graduation.bookshelf;
+import java.io.IOException;
+import java.io.ImputStraemReader;
 
 public class BookshelfTest {
-    public static void main(String[] args) {
-        
-        //graf bookshelf
+    public static void main(String[] args) throws IOException {
+        int inputMenuOption;
+        Bookshelf shelf = new Bookshelf;
 
-        System.out.println("Меню");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("     Меню");
+        System.out.println("1. ");
+        System.out.println("2. ");
+        System.out.println("3. ");
+        System.out.println("4. ");
+        System.out.println("5. ");
+        System.out.println("6. ");
+        System.out.print("выберите нужное действие и введите соответсвующий номер:");
+
+        inputMenuOption = Integer.parseInt(reader.readLine());
+        System.out.println("");
+
+        
     }
 
 }
