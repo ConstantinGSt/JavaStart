99fa4c41a91b9916da84594af22528d791e14cbb
ConstantinGSt <stepanyuk.kon@yandex.ru>
2022-07-03 16:11:33 +0700

    BookShelf graphical display

--- src/com/startjava/graduation/bookshelf/Bookshelf.java
+++ src/com/startjava/graduation/bookshelf/Bookshelf.java
@@ -5,7 +5,13 @@ public class Bookshelf {
 	Books[] bookShelf = new Books[10];
     public MyBookShelf(int inpitMenuOption) {
     	if(inpitMenuOption == 1) {
-
+    		for(int i = 0; i < length.bookShelf; i++) {
+    			if(bookShelf[i] !=null) {
+    			System.out.print("[K]");
+    			} else if(bookShelf[i] == null) {
+    				System.out.print("[]");	
+    			}
+    		}
     	} else if(inpitMenuOption == 2) {
 
     	} else if(inpitMenuOption == 3) {
@@ -15,7 +21,7 @@ public class Bookshelf {
     	} else if(inpitMenuOption == 5) {
 
     	} else if(inpitMenuOption == 6) {
-    		
+
     	}
     }
 
